af19802ab5e8058f54206df368cd2c96da43819b
John <jkforsythe@hotmail.com>
2019-12-04 22:53:35 +0000

    updated display html

--- ui.js
+++ ui.js
@@ -9,7 +9,8 @@ class UI {
       <div class="row">
         <div class="col-md-3">
           <img class="img-fluid mb-2" src="${user.avatar_url}">
-          <a href="${html_url}" target="_blank" class="btn btn-primary btn-block">View Profile</a>
+          <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block">View Profile</a>
+          <br>
         </div>
         <div class="col-md-9">
           <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>

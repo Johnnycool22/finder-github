99fc7b9c0b4f3a83c031df3259bac5e538caa931
John <jkforsythe@hotmail.com>
2019-12-04 23:40:12 +0000

    Repository data get created

--- github.js
+++ github.js
@@ -2,15 +2,21 @@ class GitHub {
   constructor() {
     this.client_id = '61c0445b211ebcce9a36';
     this.client_secret = '61f6f4ea3562994486c98751b1ac300c76768cdc';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
